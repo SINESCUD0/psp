df80a5da83ee719a144740cd08bc366041f7333f
Iván García <114983932+SINESCUD0@users.noreply.github.com>
2022-10-04 10:55:27 +0200

    Create mp1.c

---
+++ mp1.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void main(){
+
+
+	pid_t pid, pid_hijo;
+	
+	pid = fork();
+	if(pid==0){
+		system("hostname");
+	}
+	else{
+		wait(NULL);
+		printf("\nSoy el padre y mi pid es: %d \n", getpid());
+		printf("\nMi hijo es el proceso: %d \n", getpid()-1);
+	}
+	exit(0);
+}
